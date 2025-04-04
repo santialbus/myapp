ebbc196bc0f043320daf4787278a35e7a7cb4b67
santialbus <santialbus@gmail.com>
2025-04-04 16:12:18 +0000

    arreglo 10

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -72,7 +72,7 @@ class _MainScreenState extends State<MainScreen> {
                 builder: (BuildContext context, BoxConstraints constraints) {
                   final opacity =
                       (constraints.maxHeight - kToolbarHeight) /
-                      (150.0 - kToolbarHeight);
+                      (150.0 - kToolbarHeight).clamp(0.0, 1.0);
                   final bottomPadding =
                       16.0 *
                       ((constraints.maxHeight - kToolbarHeight) /
